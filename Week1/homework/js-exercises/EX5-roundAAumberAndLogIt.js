98c87ec092a8e6e3e8565b06b5fcc1ba040a1456
mas <mas.sam1368@gmail.com>
2020-10-23 18:17:15 +0330

    Exercise 5

---
+++ Week1/homework/js-exercises/EX5-roundAAumberAndLogIt.js
@@ -0,0 +1,15 @@
+'use strict';
+let z = 7.25;
+console.log(z);
+let a = Math.round(z);
+console.log(a);
+let greater;
+if (a >= z)
+{
+  greater = a;
+}
+else
+{
+  greater = z;
+}
+console.log(greater);
