20e6002c3607941aef97a260de036c4c30f61900
mas <mas.sam1368@gmail.com>
2020-10-23 13:03:53 +0330

    Exercise 1

---
+++ Week1/homework/js-exercises/helloWorld.js
@@ -0,0 +1,11 @@
+'use strict';
+console.log("Hello world!");
+console.log("دنیا سلام!");
+console.log("مرحبا بالعالم!");
+console.log("Привет мир!");
+console.log("Olá Mundo!");
+console.log("こんにちは世界!");
+console.log("silav dinya!");
+console.log("Ahoj světe!");
+console.log("สวัสดีชาวโลก!");
+console.log("Γειά σου Κόσμε!");
