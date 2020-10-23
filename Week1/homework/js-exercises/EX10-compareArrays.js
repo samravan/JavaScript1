e68262e4fc32fdc3222ae5c1f90241e39d01b145
mas <mas.sam1368@gmail.com>
2020-10-23 23:31:21 +0330

    exercise 10

---
+++ Week1/homework/js-exercises/EX10-compareArrays.js
@@ -0,0 +1,8 @@
+'use strict';
+let animal = ["birds", "monkeys", "cats", "dogs"];
+let oddNum = [1, 3, 5, 7, 9, 11, 13];
+console.log("The length of the array is " + oddNum.length);
+console.log("The length of the array is " + animal.length);
+
+if(animal.length == oddNum.length){console.log("They are the same!");}
+else{console.log("Two different size");}
