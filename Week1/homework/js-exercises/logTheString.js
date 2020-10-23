6b05ebfcd5576a7197d6d09217648e093676e45e
mas <mas.sam1368@gmail.com>
2020-10-23 18:02:26 +0330

    Exercise 4

---
+++ Week1/homework/js-exercises/logTheString.js
@@ -0,0 +1,7 @@
+'use strict';
+let myString = "Masoud S";
+console.log("This is my full name:");
+console.log(myString);
+console.log("This is my changed full name:");
+myString = "Ali Haji";
+console.log(myString);
