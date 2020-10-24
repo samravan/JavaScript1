d21b3bb17886dff7ebf90a9f8246eb455ceec071
Amirhossein <ahshadravan@gmail.com>
2020-10-24 13:44:22 +0330

    Js exercise 1 to 7 added

---
+++ Week1/homework/js-exercises/loghello.js
@@ -0,0 +1,15 @@
+
+let i = 0;
+var hello = [
+        Halo, dunia! // Indonesian,
+		
+];
+while (i<10) {
+    console.log(hello);
+    i++;
+}
+
+
+
+
+
---
+++ Week1/homework/js-exercises/stringlog.js
@@ -0,0 +1,9 @@
+ myString = "Amir Hossein Shadravan" ;
+console.log("The string is my full name");
+console.log(myString);
+ myString = "Sarah Zamandi";
+console.log("I think Sarah's name will be shown");
+console.log(myString);
+
+
+
