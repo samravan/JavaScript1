bc9a5450a02aebfd1b3292eaf6fa55150358173d
Amirhossein <ahshadravan@gmail.com>
2020-10-25 09:56:45 +0330

    Exercise 10 added

---
+++ Week1/homework/js-exercises/arrays.js
@@ -0,0 +1,11 @@
+let array1 = ["hello",123,true,"hi"];
+let array2 = [23,54,"amir","coding",232,909,"love"];
+x = array1.length;
+y = array2.length;
+console.log('The lenghth of the array is ' + x);
+console.log('The lenght of the array is ' + y);
+if (x == y) {
+    console.log("They are the same!");
+}
+
+console.log("Two diffrent sizes");
