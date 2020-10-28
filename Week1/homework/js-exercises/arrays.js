5a440227bcfdcc180f0abe95c59bba3cf39f52c9
Amirhossein <ahshadravan@gmail.com>
2020-10-28 10:01:45 +0330

    arrays.js is modified

--- Week1/homework/js-exercises/arrays.js
+++ Week1/homework/js-exercises/arrays.js
@@ -1,5 +1,5 @@
-let array1 = ["hello",123,true,"hi"];
-let array2 = [23,54,"amir","coding",232,909,"love"];
+let array1 = ["hello", 123, true, "hi"];
+let array2 = [23,54, "amir", "coding", 232,909, "love"];
 x = array1.length;
 y = array2.length;
 console.log('The lenghth of the array is ' + x);
