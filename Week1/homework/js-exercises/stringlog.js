9429def41ce63c96f453fc725f118cf446d520e3
Amirhossein <ahshadravan@gmail.com>
2020-10-28 10:40:22 +0330

    var is added

--- Week1/homework/js-exercises/stringlog.js
+++ Week1/homework/js-exercises/stringlog.js
@@ -1,7 +1,7 @@
- myString = "Amir Hossein Shadravan" ;
+var myString = "Amir Hossein Shadravan" ;
 console.log("The string is my full name");
 console.log(myString);
- myString = "Sarah Zamandi";
+var myString = "Sarah Zamandi";
 console.log("I think Sarah's name will be shown");
 console.log(myString);
 
