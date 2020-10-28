94b1f874de95e3d96727ffa4979adaa95bbc6c44
Amirhossein <ahshadravan@gmail.com>
2020-10-28 09:59:15 +0330

    animal.js is modified

--- Week1/homework/js-exercises/animals.js
+++ Week1/homework/js-exercises/animals.js
@@ -1,7 +1,7 @@
 let items = [];
 console.log("This is an empty array");
-console.log(items)
-let animals = ["Lion","Tiger","Monkey"];
+console.log(items);
+let animals = ["Lion", "Tiger", "Monkey"];
 console.log(animals);
 animals.push("Piglet");
-console.log(animals)
+console.log(animals);
