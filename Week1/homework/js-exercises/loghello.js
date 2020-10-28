408362182a5cdd14b430864f0b0b065a7cd93672
Amirhossein <ahshadravan@gmail.com>
2020-10-28 10:37:30 +0330

    loghello.js is fixed and work properly

--- Week1/homework/js-exercises/loghello.js
+++ Week1/homework/js-exercises/loghello.js
@@ -1,13 +1,15 @@
 
-let i = 0;
-var hello = [
-        Halo, dunia! // Indonesian,
-		
-];
-while (i<10) {
-    console.log(hello);
-    i++;
-}
+console.log("Hello World");
+console.log("Hallo Welt");
+console.log("Talofa lalolagi");
+console.log("नमस्ते दुनिया");
+console.log("Halo Dunia");
+console.log("مرحبا بالعالم");
+console.log("سلام دنیا");
+console.log("Dia duit ar domhan");
+console.log("salve mundi");
+console.log("Selam Dünya");
+
 
 
 
