1dca2b564d5c17469e243d4e1764a50afd18cb16
Amirhossein <ahshadravan@gmail.com>
2020-10-29 19:35:45 +0330

    exercises 3 added - needs some modification

---
+++ Week2/homework/js-exercises/recipe-ex3.js
@@ -0,0 +1,10 @@
+"use strict"
+let recipe = {
+    mealName : "Mirzaghasemi",
+    serves: 2,
+    ingredients : ["2 eggplants", "2 eggs", "1 garlic"]
+}
+for (let key in recipe) {
+    console.log(key, recipe[mealName])
+
+}        
