0cc217b63465f1aa5e0a01649cdb3c1eb8e39c63
Amirhossein <ahshadravan@gmail.com>
2020-10-30 12:58:27 +0330

    recipe-ex3 as exercise 3 added

--- Week2/homework/js-exercises/recipe-ex3.js
+++ Week2/homework/js-exercises/recipe-ex3.js
@@ -3,8 +3,24 @@ let recipe = {
     mealName : "Mirzaghasemi",
     serves: 2,
     ingredients : ["2 eggplants", "2 eggs", "1 garlic"]
-}
-for (let key in recipe) {
-    console.log(key, recipe[mealName])
+};
+//Method1:
+// for (let property in recipe) {
+//     console.log(property + ": " + recipe[property]);
+// };
 
-}        
+//Method2:
+// for (let property in recipe) {
+//     console.log(`${property}: ${recipe[property]}`);
+// };
+
+//Method3:
+let property = Object.keys(recipe);
+let value = Object.values(recipe);
+
+for (let i = 0; i < Object.keys(recipe).length; i++) {
+    
+    console.log(property[i] + ": " + value[i]);
+    
+};
+
