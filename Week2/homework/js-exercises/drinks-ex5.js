1ab6df0c3bdb1b4e7915c93e2fb52a56af94e290
Amirhossein <ahshadravan@gmail.com>
2020-10-31 17:42:51 +0330

    exercise 5 added

--- Week2/homework/js-exercises/drinks-ex5.js
+++ Week2/homework/js-exercises/drinks-ex5.js
@@ -1,10 +1,11 @@
 let drinkTray = [];
 const drinkTypes = ['cola', 'lemonade', 'water'];
+let num = 0;
 for (let i = 0; i < 5; i++) {
-    drinkTray.push(drinkTypes[i]);
-    
-    
-    
+    drinkTray.push( " " + drinkTypes[num]);
+    if (i % 2 !== 0) {
+        num++
+    } 
 };
-console.log(drinkTray)
-// console.log("Hey guys, I brought a" + ' ' +  x + "!");
+
+console.log("Hey guys, I brought a" + ' ' +  drinkTray + "!");
