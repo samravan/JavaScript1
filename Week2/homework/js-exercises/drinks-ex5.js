af143f5ccfc2ca75bdca7dca02624bfd636f5fa2
Amirhossein <ahshadravan@gmail.com>
2020-10-31 16:28:25 +0330

    drinks-ex5 as exercise 5 added, some bugs need to fix

---
+++ Week2/homework/js-exercises/drinks-ex5.js
@@ -0,0 +1,10 @@
+let drinkTray = [];
+const drinkTypes = ['cola', 'lemonade', 'water'];
+for (let i = 0; i < 5; i++) {
+    drinkTray.push(drinkTypes[i]);
+    
+    
+    
+};
+console.log(drinkTray)
+// console.log("Hey guys, I brought a" + ' ' +  x + "!");
