c3fb15be5afb4406a0d38a2776f9819d00e507e6
Amirhossein <ahshadravan@gmail.com>
2020-11-02 20:16:08 +0330

    drinks-ex5 modifed

--- Week2/homework/js-exercises/drinks-ex5.js
+++ Week2/homework/js-exercises/drinks-ex5.js
@@ -1,12 +1,17 @@
 let drinkTray = [];
 const drinkTypes = ['cola', 'lemonade', 'water'];
-let num = 0;
-for (let i = 0; i < 5; i++) {
-    drinkTray.push(drinkTypes[num]);
-    if (i % 2 !== 0) {
-        num++
-    };
-}
-
-console.log(`Hey guys, I brought a ${drinkTray}!`);
+let pointer = 0;
+let count = 0;
+while (drinkTray.length < 5) {
+  drinkTray.push(drinkTypes[pointer])
+  count++;
+  if (count ==2) {
+    pointer++;
+    count = 0;
+  };
+  if (pointer >= drinkTypes.length) {
+    pointer = 0;
+  };
+};
+console.log(drinkTray);
 
