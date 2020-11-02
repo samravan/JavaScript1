ff87e3bba7ae99b7c4e115fcd60d1f4902d7f500
mas <mas.sam1368@gmail.com>
2020-11-02 21:25:33 +0330

    Fixed exercise 5

--- Week2/homework/Masoud/js-exercises/EX5-whoWantsADrink.js
+++ Week2/homework/Masoud/js-exercises/EX5-whoWantsADrink.js
@@ -3,13 +3,20 @@
 const drinkTray = [];
 const drinkTypes = ['cola', 'lemonade', 'water'];
 let num = 0;
+let courser = 0;
 
 for(let i = 0 ; i < 5 ; i++) {
 
   drinkTray.push(drinkTypes[num]);
+  courser++;
 
-  if(i % 2 !== 0){
+  if(courser == 2){
     num++;
+    courser = 0;
+  }
+
+  if (num == drinkTypes.length){
+    num = 0;
   }
 }
 
