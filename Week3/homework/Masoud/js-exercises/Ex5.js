5f215bc43b5a45193d21d495b4260c0acb8764dd
mas <mas.sam1368@gmail.com>
2020-11-06 13:18:46 +0330

    Ex5

--- Week3/homework/Masoud/js-exercises/Ex5.js
+++ Week3/homework/Masoud/js-exercises/Ex5.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const cardForParty = {
+  beers : 2.22,
+  bread : 1.8,
+  tea : 4,
+  cola : 8,
+  tomato : 3
+}
+
+function calculateTotalPrice(card) {
+  let totalPrice = 0;
+  for (let property in card){
+    totalPrice += card[property];
+  }
+  return `Total: \u20ac${totalPrice}`;
+}
+
+console.log(calculateTotalPrice(cardForParty));
