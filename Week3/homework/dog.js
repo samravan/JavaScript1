0f664fd7cff332675f20c19ccab5cdd7b9c61489
Amirhossein <ahshadravan@gmail.com>
2020-11-06 23:12:35 +0330

    Exercise 2 added

---
+++ Week3/homework/dog.js
@@ -0,0 +1,7 @@
+function calculateDogAge(age) {
+    const doggyage = age * 7;
+    console.log(doggyage); //still don't know how can I use return insted of console!
+};
+calculateDogAge(3);
+calculateDogAge(2);
+calculateDogAge(4);
