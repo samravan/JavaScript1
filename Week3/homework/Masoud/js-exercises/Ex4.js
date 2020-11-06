fd539ec51d114aaf58d191220703b6f23544d8e6
mas <mas.sam1368@gmail.com>
2020-11-06 12:50:22 +0330

    Ex4

--- Week3/homework/Masoud/js-exercises/Ex4.js
+++ Week3/homework/Masoud/js-exercises/Ex4.js
@@ -1,1 +1,20 @@
 'use strict'
+
+const shoppingCard = ['banana', 'milk'];
+
+function addToShoppingCard(add) {
+
+  if (shoppingCard.length <= 2){
+    shoppingCard.push(add);
+  } else {
+    shoppingCard.shift();
+    shoppingCard.push(add);
+  }
+  const shoppingCardString = shoppingCard.join(', ');
+  return `You bought ${shoppingCardString}!`;
+}
+
+console.log(addToShoppingCard('bread'));
+console.log(addToShoppingCard('tomato'));
+console.log(addToShoppingCard('rice'));
+
