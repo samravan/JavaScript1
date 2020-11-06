fefc7f994b6761f01dbb6b420c5c2e41fac7a5a8
mas <mas.sam1368@gmail.com>
2020-11-06 12:35:44 +0330

    Ex2

--- Week3/homework/Masoud/js-exercises/Ex2.js
+++ Week3/homework/Masoud/js-exercises/Ex2.js
@@ -1,1 +1,10 @@
 'use strict'
+
+function calculateDogAge(age) {
+  const dogAge = age * 7;
+  return `your doggie is ${dogAge} years old in dog years!`
+}
+
+console.log(calculateDogAge(3));
+console.log(calculateDogAge(5));
+console.log(calculateDogAge(8));
