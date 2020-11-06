58db22684cbcc737012a031e9dd556a5df66b7a5
mas <mas.sam1368@gmail.com>
2020-11-06 11:22:25 +0330

    Ex1

--- Week3/homework/Masoud/js-exercises/Ex1.js
+++ Week3/homework/Masoud/js-exercises/Ex1.js
@@ -1,1 +1,11 @@
 'use strict'
+
+function giveCompliment(name) {
+  const compliments = ['awesome', 'smart', 'perfect', 'strong', 'gratful', 'gorgeous', 'helpful', 'clever', 'handsome', 'great', 'cute'];
+  const selectOneCompliment = compliments[Math.round(Math.random()*10)];
+  return `You are ${selectOneCompliment}, ${name}!`;
+}
+
+console.log(giveCompliment('Masoud'));
+console.log(giveCompliment('Masoud'));
+console.log(giveCompliment('Masoud'));
