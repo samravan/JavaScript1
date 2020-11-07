bfcd362a810526f1c22f6c6c06826678f822fb0d
Amirhossein <ahshadravan@gmail.com>
2020-11-07 12:40:09 +0330

    Shopping-exe4 as exercise 4 added - noer-ex1 is modified and the soloution has changed

--- Week3/homework/js-exercises/noer-ex1.js
+++ Week3/homework/js-exercises/noer-ex1.js
@@ -1,14 +1,15 @@
 'use strict'
+const compliments = ["great", "amazing", "wonderful", "lamentable", "attractive", "merciful", "fair" , "spectacular", "dynamic", "elegant"];
 function giveCompliment(name) {
-    const compliments = ["great", "amazing", "wonderful", "lamentable", "attractive", "merciful", "fair" , "spectacular", "dynamic", "elegant"];
+    
     const randCompliment = compliments[Math.floor(Math.random() * compliments.length)];
-
-    return `You are ${randCompliment}, ${name}!`; //I've tried to use "return" without console.log but it didn't return anything! Why?
+    const result = console.log(`You are ${randCompliment}, ${name}!`)
+    return result; //I've tried to use "return" without console.log but it didn't return anything! Why?
 };
-// giveCompliment('ali')
-const x = giveCompliment('ali');
-const y = giveCompliment('ali');
-const z = giveCompliment('ali');
-console.log(x);
-console.log(y);
-console.log(z);
+giveCompliment('ali')
+// const x = giveCompliment('ali');
+// const y = giveCompliment('ali');
+// const z = giveCompliment('ali');
+// console.log(x);
+// console.log(y);
+// console.log(z);
---
+++ Week3/homework/js-exercises/shopping-exe4.js
@@ -0,0 +1,15 @@
+'use strict'
+const shoppingCart = ['bananas', 'milk'];
+function addToShoppingCart(item){
+    shoppingCart.push(item);
+    for(let i = 0; i < shoppingCart.length; i++) {
+    if (shoppingCart.length > 3) {
+        shoppingCart.shift();
+    }
+};
+    const result = console.log(`You bought ${shoppingCart}!`)
+    return result;
+};
+addToShoppingCart('chocolate');
+addToShoppingCart('waffles');
+addToShoppingCart('tea');
