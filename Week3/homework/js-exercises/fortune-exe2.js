31403da4527d19a6720e8c95bcf61df1d7ed1d29
Amirhossein <ahshadravan@gmail.com>
2020-11-07 12:15:05 +0330

    forune-exe2 as exercise3 added

---
+++ Week3/homework/js-exercises/fortune-exe2.js
@@ -0,0 +1,16 @@
+const numChildren = [1, 2, 3, 4, 5];
+const partnerNames = ['Ali', 'Amir', 'Lili', 'Mina', 'Masood'];
+const locations = ['Iran', 'USA', 'Holland', 'UK', 'Jordan'];
+const jobs = ['Doctor', 'Programmer', 'Killer', 'Police', 'President'];
+function tellFortune(numChildren, partnerNames, locations, jobs){
+    const randomNum = numChildren[Math.floor(Math.random() * numChildren.length)];
+    const randomName = partnerNames[Math.floor(Math.random() * partnerNames.length)];
+    const randomLocation = locations[Math.floor(Math.random() * locations.length)];
+    const randomJob = jobs[Math.floor(Math.random() * jobs.length)];
+
+    console.log(`You will be a ${randomJob} in ${randomLocation}, married to ${randomName} with ${randomNum} kids.`)
+};
+
+tellFortune(numChildren, partnerNames, locations, jobs);
+tellFortune(numChildren, partnerNames, locations, jobs);
+tellFortune(numChildren, partnerNames, locations, jobs);
