e4849c82c6872c44574f08b0b495bfb1f9ac688d
Amirhossein <ahshadravan@gmail.com>
2020-11-11 20:55:05 +0330

    Errores are removed

--- Week3/homework/js-exercises/creditCardProject.js
+++ Week3/homework/js-exercises/creditCardProject.js
@@ -8,26 +8,27 @@ function validateCreditNumber(num) {
     //At least two diffrent numbers should be represented
     function similarity(array){
             let firstItem = array[0]
-            let x = array.filter(elements => elements == firstItem).length != array.length ? true : false;
+            let x = array.filter(elements => elements == firstItem).length != array.length ? false : true;
             return x 
         }
 
         //The Sum of all the numbers must be greater than 16:
         let sum = 0
+        const lastNum = array[array.length -1];
         for (let i = 0; i < array.length; i++){
                 let arrayInt = parseInt(array[i])
                 sum += arrayInt
             }
 
-    switch(false){
+    switch(true){
 
         //The length of the number
-        case num.length == 16:
+        case num.length !== 16:
             console.log(`Invalid! The input ${num} should be 16 characters!`);
             break;
 
         //All characters must be numbers
-        case num.match(/^[0-9]+$/) != null:
+        case num.match(/^[0-9]+$/) == null:
             console.log(`Invalid! The input ${num} should contain only numbers!`);
             break;
 
@@ -37,12 +38,13 @@ function validateCreditNumber(num) {
             break;
 
         //The last number must be even
-        case array.pop() % 2 == 0:
+        case lastNum % 2 !== 0:
             console.log(`Invalid! The last number of the  input ${num} should be even`);
+            
             break;
         
         //The Sum of all the numbers must be greater than 16:
-        case sum > 16:
+        case sum < 16:
             console.log(`Invalid! The sum of the input ${num} should be more than 16`);
             break;
 
@@ -54,6 +56,7 @@ function validateCreditNumber(num) {
 };
 
 
-validateCreditNumber('6666666666661666');
+validateCreditNumber('66666666666561666');
 validateCreditNumber('a92332119c011112');
-validateCreditNumber('4444444444444444');
+validateCreditNumber('4444444444444441');
+validateCreditNumber('1111111111111110');
