d31790d14906f3892aadb6dd235fefd59be85677
mas <mas.sam1368@gmail.com>
2020-11-13 17:51:54 +0330

    Fixed returs

--- Week3/homework/Masoud/creditCardValidator/creditCardValidator.js
+++ Week3/homework/Masoud/creditCardValidator/creditCardValidator.js
@@ -6,7 +6,7 @@ const validateCreditNumber = (creditCardNumber) => {
   //Input must be 16 characters
   function checkLength(creditCardNumber) {
     if (creditCardNumber.length !== 16){
-      return `Invalid! The input ${creditCardNumber} should contains 16 characters!`;
+      return false;
     } else {
       return true;
     }
@@ -16,7 +16,7 @@ const validateCreditNumber = (creditCardNumber) => {
   function checkType(creditCardNumber) {
     for (let i = 0 ; i < creditCardNumber.length ; i++) {
       if (!creditCardNumber[i].match(/[0-9]/g)) {
-        return `Invalid! The input ${creditCardNumber} should contains only numbers!`;
+        return false;
       }
     }
     return true;
@@ -29,7 +29,7 @@ const validateCreditNumber = (creditCardNumber) => {
         return true;
       }
     }
-   return `Invalid! The input ${creditCardNumber} should contains at least 2 different types of numbers!`;
+   return false;
   }
 
   //The last number must be even
@@ -37,7 +37,7 @@ const validateCreditNumber = (creditCardNumber) => {
     if (creditCardNumber[creditCardNumber.length-1] % 2 == 0) {
       return true;
     } else {
-      return `Invalid! The last number must be even`;
+      return false;
     }
   }
 
@@ -48,34 +48,46 @@ const validateCreditNumber = (creditCardNumber) => {
      sum += parseInt(creditCardNumber[i]);
    }
    if (sum < 16) {
-     return `Invalid! The input ${creditCardNumber} should contains the numbers that must be greater than 16! `
+     return false;
    } else {
      return true;
    }
  }
 
 
- //Call and console all Functions
-  const array = [];
-  const array2 = [];
-  array.push(checkLength(creditCardNumber));
-  array.push(checkType(creditCardNumber));
-  array.push(atLeastTwo(creditCardNumber));
-  array.push(mustEven(creditCardNumber));
-  array.push(sumOf(creditCardNumber));
+
+//Call and console all Functions
+const array = [];
+
+if (!checkLength(creditCardNumber)){
+  array.push(`Invalid! The input ${creditCardNumber} should contains 16 characters!`);
+}
+
+if (!checkType(creditCardNumber)){
+  array.push(`Invalid! The input ${creditCardNumber} should contains only numbers!`);
+}
+
+if (!atLeastTwo(creditCardNumber)){
+  array.push(`Invalid! The input ${creditCardNumber} should contains at least 2 different types of numbers!`);
+}
+
+if (!mustEven(creditCardNumber)){
+  array.push(`Invalid! The last number must be even`);
+}
+
+if (!sumOf(creditCardNumber)){
+  array.push(`Invalid! The input ${creditCardNumber} should contains the numbers that must be greater than 16!`);
+}
+
+if (array.length == 0){
+   console.log(`Success! The input ${creditCardNumber} is a valid credit card number!`)
+} else {
   for (let i = 0 ; i < array.length ; i++) {
-    if (array[i] !== true) {
-      console.log(array[i]);
-    } else {
-      array2.push(array[i]);
-    }
+    console.log(array[i]);
   }
- 
-  if (array2.length == 5){
-    console.log(`Success! The input ${creditCardNumber} is a valid credit card number!`);
-  }
+}
 };
 
-const creditCardNumber = '2222222222222224'; //Sample credit card number!
+const creditCardNumber = '1111111111111112'; //Sample credit card number!
 validateCreditNumber(creditCardNumber); //Call function
 
